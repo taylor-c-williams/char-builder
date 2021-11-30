cb8eb9511c716bbeb0279b3d77b4acf7aa1ae8b8
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-11-29 17:27:46 -0800

    basic scaffolding installed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Character from './Components/Character/Character';
+import Display from './Components/Display/Display';
+import Picker from './Components/Picker/Picker';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [head, setHead] = useState('dog');
+	const [middle, setMiddle] = useState('fancy');
+	const [newCatchphrase, setNewCatchphrase] = useState('');
+	const [catchphrases, setCatchphrases] = useState([]);
+
+	const handleClick = () => {
+		setCatchphrases((prevState) => [...prevState, newCatchphrase]);
+	};
+
+	return (
+		<container className="App">
+			<h1>Char Designer</h1>
+		</container>
+	);
 }
 
 export default App;
