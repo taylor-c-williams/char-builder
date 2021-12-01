1d2d5f8fc5befb05ff6c2ca0f9a398c2a406e8cf
TAYLOR CALLAN WILLIAMS <taylor.callan.williams@gmail.com>
2021-11-30 16:48:58 -0800

    character.test passing

--- src/Components/Character/Character.test.jsx
+++ src/Components/Character/Character.test.jsx
@@ -4,7 +4,9 @@ import Character from './Character';
 it('Should render Character component', () => {
 	const { container } = render(<Character head="dog" middle="fancy" />);
 	const head = screen.getByLabelText(/head/);
+	const middle = screen.getByLabelText(/middle/);
 
-	expect(head).toHaveStyle(`background-image: url(./dog-head.png`);
+	expect(head).toHaveStyle(`background-image: url(./dog-head.png)`);
+	expect(middle).toHaveStyle(`background-image: url(./fancy-middle.png)`);
 	expect(container).toMatchSnapshot();
 });
